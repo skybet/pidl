7a783ad7f89a52e7f8e733f4c88c92c352dc5dd5
Craig Andrews <craig.andrews@bskyb.com>
2014-03-19 11:40:04 +0000

    [refs MAX-649] Add custom context handlers

--- lib/pidl/context.rb
+++ lib/pidl/context.rb
@@ -7,14 +7,16 @@ module Pidl
   # exposing a shared key/value set.
   class Context
 
-    attr_reader :params
-
     # Create a new, empty key/value set
     def initialize(flags = nil)
       flags ||= {}
-      @params = flags[:params] || []
       @context = {}
       @mutex = Mutex.new
+
+      # Create custom accessors for the flags
+      flags.each { |name, value|
+        create_accessor_method name, value
+      }
     end
 
     # Store the given key/from the
@@ -44,6 +46,27 @@ module Pidl
     # and values of the context
     def all
       @context
+    end
+
+
+    private
+
+    def create_accessor_method name, value
+      var_name = "@#{name}".to_sym
+      instance_variable_set var_name, value 
+      if value.is_a? Hash
+        define_singleton_method name do |key|
+          v = instance_variable_get var_name
+          if v[key].nil?
+            raise KeyError.new "Key #{key} does not exist in #{name}"
+          end
+          v[key]
+        end
+      else
+        define_singleton_method name do
+          instance_variable_get var_name
+        end
+      end
     end
 
   end
